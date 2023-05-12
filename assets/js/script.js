9b7a4a5729ec177bb1a34ea2d41bc69f1bdac625
cwchilvers <cwchilvers@gmail.com>
2023-05-11 19:49:03 -0700

    Added a function to scrape lyrics from Genius and diplay in web page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,30 +6,62 @@ const releaseDateEl = document.getElementById("release-date");
 const lyricsEl = document.getElementById("lyrics");
 const sampleInfoEl = document.getElementById("sample-info");
 
-// Genius API
-const url = "https://api.genius.com/";
-const search = "search?q=";
-const token = "0mnLuVjVhj3q71HPoO4qeXl5XHowzNHG87dc1Vr9VfLCKYy_apxIFSsGRajaCKh9";
+// APIs
+    // Genius API
+const genURL = "https://api.genius.com/";
+const genToken = "0mnLuVjVhj3q71HPoO4qeXl5XHowzNHG87dc1Vr9VfLCKYy_apxIFSsGRajaCKh9";
+const genSearch = "search?q=";
+    // CORS Proxy API
+const corsProxyURL = "https://api.codetabs.com/v1/proxy?quest=";    // This is to get around CORS policy error
 
-// Search parameters
+// User input
 var song = "all star"
 var artist = "smash mouth";
 
+// Misc.
+var lyricsURL;
+var lyrics;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 SearchGenius();
 
+
+
+// Get data from Genius API
 function SearchGenius() {
-    fetch(url + search + song + " " + artist + "&access_token=" + token)
+    fetch(genURL + genSearch + song + " " + artist + "&access_token=" + genToken)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.response.hits[0].result);
-
             // Update page content
-            songEl.textContent = data.response.hits[0].result.title;                                                    // Song Title
-            artistEl.textContent = "by " + data.response.hits[0].result.artist_names;                                   // Artist
-            coverArtEl.setAttribute("src", data.response.hits[0].result.header_image_url);                              // Cover Art
-            releaseDateEl.textContent = data.response.hits[0].result.release_date_for_display;                          // Release Date
-            lyricsEl.setAttribute("href", "https://genius.com" + data.response.hits[0].result.path)                     // Lyrics (External URL)
+            songEl.textContent = data.response.hits[0].result.title;                            // Song Title
+            artistEl.textContent = "by " + data.response.hits[0].result.artist_names;           // Artist
+            coverArtEl.setAttribute("src", data.response.hits[0].result.header_image_url);      // Cover Art
+            releaseDateEl.textContent = data.response.hits[0].result.release_date_for_display;  // Release Date
+            // URL for lyrics
+            lyricsURL = "https://genius.com" + data.response.hits[0].result.path;
+            GetLyrics();
         });
+}
+
+// Scrape lyrics from Genius
+function GetLyrics() {
+    $.get(corsProxyURL + lyricsURL, function(html) {
+        lyrics = ($(html).find('.Lyrics__Container-sc-1ynbvzw-5').html());  // 
+        lyricsEl.innerHTML = lyrics;                                        // 
+        $("#lyrics > a").removeAttr("href");                                // Remove all hrefs from Genius lyrics
+    });
 }
